28cdf55a06b230269e106d8115e1b063844e18bb
Kwonputer <thtm9020@gmail.com>
2022-01-17 17:13:55 +0900

    first_update

---
+++ app/src/main/java/com/kwon/capital_gains_tax/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kwon.capital_gains_tax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
